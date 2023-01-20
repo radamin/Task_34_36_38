627786a4564e387d06f1131d0e8d118b2d5e4089
Ramazan Daminov <radamin@yandex.ru>
2023-01-20 14:44:46 +0300

    Added Task34

---
+++ Task34/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+
+int[] CreateArray(int size)
+{
+  int[] array = new int[size];
+  Random rnd = new Random();
+  for (int i = 0; i < array.Length; i++)
+  {
+    array[i] = rnd.Next(100, 1000);
+  }
+  return array;
+}
+
+void PrintArray (int[] arr)
+{
+  Console.Write("[");
+  for (int i = 0; i < arr.Length; i++)
+  {
+    if(i < arr.Length - 1) Console.Write(arr[i] + ",");
+    else Console.Write(arr[i]);
+  }
+  Console.WriteLine("]");
+}
+
+int QuantityPositive(int[] arr)
+{
+  int quantity = 0;
+  for (int i = 0; i < arr.Length; i++)
+  {
+    if (arr[i] % 2 == 0)
+    {
+      quantity++;
+    }
+  }
+  return quantity;
+}
+
+int[] array = CreateArray(6);
+PrintArray(array);
+Console.WriteLine();
+
+int quantity = QuantityPositive(array);
+Console.WriteLine($"The number of even numbers in the array: {quantity}");
